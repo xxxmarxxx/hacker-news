c052375d9b861cbd8c26733ecde5e258e1a9521a
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-02-11 14:22:32 +0100

    L 225

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import SearchForm from "./SearchForm";
 import Stories from "./Stories";
 import Buttons from "./Buttons";
-// lesson 219/220/221/222/223/224
+// lesson 219/220/221/222/223/224/225/225
 function App() {
   return (
     <>
